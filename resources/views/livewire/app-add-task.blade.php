55734dfb8100c61ac7d9ef9296be8bf00ad4aee7
unknown <ahmohsen900@gmail.com>
2024-03-02 20:14:14 +0200

    first commit

---
+++ resources/views/livewire/app-add-task.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <h3 class="text-center">Add New Task</h3>
+    @if (session()->has('message'))
+    <div class="div alert alert-success">
+    {{session('message') }}
+    </div>
+    @endif  
+@error('title')
+<div class="div alert alert-danger">
+    {{$message }}
+    </div>
+@enderror
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" wire:model="title" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <button wire:click.prevent="addTask" class="btn btn-primary btn-block">Add</button>
+    </div>
+    </form>
+</div>
